a688b47b0400ee8d37d0e1088075cdc1e2fdcaa0
RudhyDev <rudhy.pereira@gmail.com>
2021-07-16 22:59:43 -0300

    fim da aula

--- Aula01-12-07-2021/Portfolio/assets/JS/script.js
+++ Aula01-12-07-2021/Portfolio/assets/JS/script.js
@@ -1,4 +1,7 @@
 let inputNome = document.querySelector('#nome')
+let inputEmail = document.querySelector('#email')
+let textArea = document.querySelector("#mensagem")
+let btnEnviar = document.querySelector('#enviar')
 
 
 inputNome?.addEventListener('keyup', ()=>{
@@ -9,3 +12,22 @@ inputNome?.addEventListener('keyup', ()=>{
     }
 })
 
+inputEmail?.addEventListener('keyup', ()=>{
+    if(inputEmail.value.indexOf('@') ==-1 || inputEmail.value.indexOf('.') ==-1){
+        inputEmail.style.borderColor ='red'
+    } else {
+        inputEmail.style.borderColor ='green'
+    }
+})
+
+textArea?.addEventListener('keyup', ()=>{
+    if(textArea.value.length > 100){
+        textArea.style.borderColor ='red'
+    } else {
+        textArea.style.borderColor ='green'
+    }
+})
+
+btnEnviar.addEventListener('click', ()=>{
+    
+})
