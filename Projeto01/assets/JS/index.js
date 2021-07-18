ecf642cda1400c4c15556cafccab7727418dd2b0
RudhyDev <rudhy.pereira@gmail.com>
2021-07-18 14:47:06 -0300

    Finalizado, porem com uma resalva!

--- Projeto01/assets/JS/index.js
+++ Projeto01/assets/JS/index.js
@@ -5,19 +5,19 @@ let elementoBtn = document.querySelector('#humor')
 /* Adiciona o evento de click no elementoBtn e executa a função anonima (arrow function) */
 elementoBtn.addEventListener('click', ()=>{
     if(elementoBtn.value == 'primeiro'){
-        elementoImg.src = './../img/SemAlma.jpg'
+        elementoImg.src = 'C:/Users/Trabalho/Desktop/BLUE/Mod02/Projeto01/assets/img/SemAlma.jpg'
         elementoSituacao.innerText = 'Sem Alma!'
         elementoBtn.value = 'segundo'
     } else if(elementoBtn.value == 'segundo'){
-        elementoImg.src = './../img/Puto.jpeg'
+        elementoImg.src = 'C:/Users/Trabalho/Desktop/BLUE/Mod02/Projeto01/assets/img/Puto.jpeg'
         elementoSituacao.innerText = 'Puto!'
         elementoBtn.value = 'terceiro'
     } else if(elementoBtn.value == 'terceiro'){
-        elementoImg.src = './../img/host.jpg'
+        elementoImg.src = 'C:/Users/Trabalho/Desktop/BLUE/Mod02/Projeto01/assets/img/host.jpg'
         elementoSituacao.innerText = 'Galante'
         elementoBtn.value = 'quarto'
     } else if(elementoBtn.value == 'quarto'){
-        elementoImg.src = './../img/CaraDeCaguei.jpg'
+        elementoImg.src = 'C:/Users/Trabalho/Desktop/BLUE/Mod02/Projeto01/assets/img/CaraDeCaguei.jpg'
         elementoSituacao.innerText = 'Caguei pra você'
         elementoBtn.value = 'primeiro'
     }
