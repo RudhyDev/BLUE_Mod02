1bf927612ed155574fb9f405d7ad7d570694c32e
RudhyDev <rudhy.pereira@gmail.com>
2021-07-20 22:08:09 -0300

    Algumas alterações no portfolio

--- Aula01-12-07-2021/Portfolio/assets/JS/script.js
+++ Aula01-12-07-2021/Portfolio/assets/JS/script.js
@@ -2,33 +2,50 @@ let inputNome = document.querySelector('#nome')
 let inputEmail = document.querySelector('#email')
 let textArea = document.querySelector("#mensagem")
 let btnEnviar = document.querySelector('#enviar')
+let nomeOk = false
+let emailOk = false
+let msgOk = false
 
 
-inputNome?.addEventListener('keyup', ()=>{
+btnEnviar.disabled = true
+
+inputNome.addEventListener('keyup', ()=>{
     if(inputNome.value.length < 3){
         inputNome.style.borderColor = 'red'
     } else {
+        nomeOk = true
         inputNome.style.borderColor = 'green'
+        
     }
 })
 
-inputEmail?.addEventListener('keyup', ()=>{
+inputEmail.addEventListener('keyup', ()=>{
     if(inputEmail.value.indexOf('@') ==-1 || inputEmail.value.indexOf('.') ==-1){
         inputEmail.style.borderColor ='red'
     } else {
+        emailOk = true
         inputEmail.style.borderColor ='green'
+        
     }
 })
 
-textArea?.addEventListener('keyup', ()=>{
+textArea.addEventListener('keyup', ()=>{
     if(textArea.value.length > 500){
         textArea.style.borderColor ='red'
     } else {
+        msgOk = true
         textArea.style.borderColor ='green'
     }
 })
 
-btnEnviar.addEventListener('click', ()=>{
-    // copiar aquela função aqui do botão
-})
+
+if(nomeOk == true && emailOk == true && msgOk == true) {
+    btnEnviar.disabled = false
+    btnEnviar.addEventListener('click', ()=>{
+        alert('Formulário enviado!')
+    })
+} else {
+    console.log('   ERRROU')
+}
+
 
