14d54baa9e50dad5198b04187f67599cfe373e33
RudhyDev <rudhy.pereira@gmail.com>
2021-07-20 22:55:31 -0300

    Tentando arrumar a validação do botão enviar

--- Aula01-12-07-2021/Portfolio/assets/JS/script.js
+++ Aula01-12-07-2021/Portfolio/assets/JS/script.js
@@ -6,46 +6,42 @@ let nomeOk = false
 let emailOk = false
 let msgOk = false
 
-
 btnEnviar.disabled = true
+
 
 inputNome.addEventListener('keyup', ()=>{
     if(inputNome.value.length < 3){
         inputNome.style.borderColor = 'red'
+        nomeOk = false
     } else {
-        nomeOk = true
         inputNome.style.borderColor = 'green'
-        
+        nomeOk = true
     }
 })
 
 inputEmail.addEventListener('keyup', ()=>{
     if(inputEmail.value.indexOf('@') ==-1 || inputEmail.value.indexOf('.') ==-1){
         inputEmail.style.borderColor ='red'
+        emailOk = false
     } else {
-        emailOk = true
         inputEmail.style.borderColor ='green'
-        
+        emailOk = true
     }
 })
 
 textArea.addEventListener('keyup', ()=>{
     if(textArea.value.length > 500){
         textArea.style.borderColor ='red'
+        msgOk = false
     } else {
-        msgOk = true
         textArea.style.borderColor ='green'
+        msgOk = true
     }
 })
 
-
-if(nomeOk == true && emailOk == true && msgOk == true) {
+if(nomeOk == true && emailOk == true && msgOk == true){
     btnEnviar.disabled = false
-    btnEnviar.addEventListener('click', ()=>{
-        alert('Formulário enviado!')
-    })
-} else {
-    console.log('   ERRROU')
+    alert('Mensagem envida!')
+} else{
+    btnEnviar.disabled = true
 }
-
-
